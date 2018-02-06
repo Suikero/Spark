50159103bc868db7677d54831d79e1e5ae398e68
Suikero <katiskoski95@gmail.com>
2018-02-07 00:30:00 +0200

    Initial commit

---
+++ app/src/main/java/spark/suikero/spark/MainActivity.java
@@ -0,0 +1,57 @@
+package spark.suikero.spark;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> list = new ArrayList<>();
+    EditText textFirst;
+    ArrayAdapter <String> adapter;
+    ListView listFirst;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textFirst = findViewById(R.id.editText);
+        listFirst = findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        listFirst.setAdapter(adapter);
+
+        String text = "aas";
+        String asd = "ads";
+        String juu = "asd";
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        return true;
+
+    }
+
+    public void addItem (View view){
+
+        list.add(textFirst.getText().toString());
+        adapter.notifyDataSetChanged();
+
+    }
+
+
+
+
+}
